97e395c14f185c8b5af1d30a7b4430ff60d57834
leonardoLimaDeSouzaLopes <91790685+leonardoLimaDeSouzaLopes@users.noreply.github.com>
2024-04-25 12:14:40 -0300

    Fiz o ddl e comecei o dml

---
+++ ddl.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE bdEscolaIdiomas
+GO
+
+--USE master
+--DROP DATABASE bdEstoque
+
+USE bdEscolaIdiomas
+
+
+CREATE TABLE tbCurso (
+	codCurso INT PRIMARY KEY IDENTITY (1, 1)
+	, nomeCurso VARCHAR(50)
+	, cargaHorariaCurso INT
+	, valorCurso MONEY
+)
+
+--DROP TABLE tbCurso
+
+
+CREATE TABLE tbTurma (
+	codTurma INT PRIMARY KEY IDENTITY (1, 1)
+	, nomeTurma VARCHAR(30)
+	, codCurso INT FOREIGN KEY REFERENCES tbCurso(codCurso)
+	, horarioTurma DATETIME
+)
+
+--DROP TABLE tbTurma
+
+CREATE TABLE tbAluno (
+	codAluno INT PRIMARY KEY IDENTITY (1, 1)
+	, nomeAluno VARCHAR(200)
+	, dataNascAluno DATETIME
+	, rgAluno CHAR(11)
+	, naturalidadeAluno CHAR(2)
+)
+
+--DROP TABLE tbAluno
+
+CREATE TABLE tbMatricula (
+	codMatricula INT PRIMARY KEY IDENTITY (1, 1)
+	, dataMatricula DATETIME
+	, codAluno INT FOREIGN KEY REFERENCES tbAluno(codAluno)
+	, codTurma INT FOREIGN KEY REFERENCES tbTurma(codTurma)
+)
+
+--DROP TABLE tbMatricula
---
+++ dml.sql
@@ -0,0 +1,10 @@
+USE bdEscolaIdiomas
+
+INSERT INTO tbCurso
+	(nomeCurso, cargaHorariaCurso, valorCurso)
+
+VALUES
+	( 'Ingles' , 2000 , 356.00 )
+	, ( 'Alemao' , 3000 , 478.00 )
+	, ( 'Espanha' , 4000 , 500 )
+	
