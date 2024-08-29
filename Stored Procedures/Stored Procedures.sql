80881fec2e9197e53bc496b1761d072f0dd05143
leonardoLimaDeSouzaLopes <91790685+leonardoLimaDeSouzaLopes@users.noreply.github.com>
2024-08-29 11:02:16 -0300

    Fizemos a 2° Stored Procedures

--- Stored Procedures/Stored Procedures.sql
+++ Stored Procedures/Stored Procedures.sql
@@ -10,3 +10,16 @@ AS
 		WHERE codAluno = @codAluno)
 
 	PRINT('Nome do Aluno = ' + @nomeAluno + 'Data Nascimento do Aluno = ' + CONVERT(VARCHAR(10), @dataNascAluno))
+
+--2)
+CREATE PROCEDURE Insere_Aluno
+AS
+	PRINT('Insira os dados do Aluno: Nome, data de nascimento, rg e naturalidade')
+	DECLARE @nomeAluno VARCHAR(200)
+	DECLARE @dataNascAluno DATE
+	DECLARE @rgAluno CHAR(12)
+	DECLARE @naturalidadeAluno CHAR(2)
+
+	INSERT INTO tbAluno (nomeAluno,dataNascAluno,rgAluno,naturalidadeAluno) VALUES (@nomeAluno,@dataNascAluno,@rgAluno,@naturalidadeAluno)
+
+	PRINT(' Aluno ' + @nomeAluno + ' cadastrado com sucesso!')
