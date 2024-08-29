725928419a43fff36df1a1bd5d57a48579a9e855
leonardoLimaDeSouzaLopes <91790685+leonardoLimaDeSouzaLopes@users.noreply.github.com>
2024-08-29 12:13:02 -0300

    Tentamos fazer mais Exercícios
    Tentamos fazer mais exercícios porém só o primeiro está funcionando

--- Stored Procedures/Stored Procedures.sql
+++ Stored Procedures/Stored Procedures.sql
@@ -1,8 +1,11 @@
+USE bdEscolaIdiomas
+
+--1)
 CREATE PROCEDURE spBusca_Aluno
 @codAluno INT
 AS
-	DECLARE @nomeAluno INT
-	DECLARE @dataNascAluno SMALLDATETIME
+	DECLARE @nomeAluno VARCHAR(200)
+	DECLARE @dataNascAluno DATE
 
 	SET @nomeAluno = (SELECT nomeAluno AS 'NomeAluno' FROM tbAluno
 		WHERE codAluno = @codAluno)
@@ -12,14 +15,54 @@ AS
 	PRINT('Nome do Aluno = ' + @nomeAluno + 'Data Nascimento do Aluno = ' + CONVERT(VARCHAR(10), @dataNascAluno))
 
 --2)
-CREATE PROCEDURE Insere_Aluno
+CREATE PROCEDURE spInsere_Aluno
+	@nomeAluno VARCHAR(200)
+	, @dataNascAluno DATE
+	, @rgAluno CHAR(12)
+	, @naturalidadeAluno CHAR(2)
 AS
-	PRINT('Insira os dados do Aluno: Nome, data de nascimento, rg e naturalidade')
-	DECLARE @nomeAluno VARCHAR(200)
-	DECLARE @dataNascAluno DATE
-	DECLARE @rgAluno CHAR(12)
-	DECLARE @naturalidadeAluno CHAR(2)
-
 	INSERT INTO tbAluno (nomeAluno,dataNascAluno,rgAluno,naturalidadeAluno) VALUES (@nomeAluno,@dataNascAluno,@rgAluno,@naturalidadeAluno)
 
-	PRINT(' Aluno ' + @nomeAluno + ' cadastrado com sucesso!')
+	PRINT('Aluno ' + @nomeAluno + 'Cadastrado Com Sucesso')
+
+--3)
+CREATE PROCEDURE Aumento_Preco
+@valorCurso
+AS
+
+	DECLARE @nomeCurso VARCHAR(50)
+	DECLARE @porcentualCurso INT
+
+	SET @valorCurso = (SELECT valorCurso FROM tbCurso
+		WHERE @nomeCurso = nomeCurso) * @porcentualCurso 
+
+	PRINT(@valorCurso)
+	
+
+--4)
+CREATE PROCEDURE Exibe_Turma
+@nomeTurma
+AS
+	DECLARE @nomeTurma VARCHAR(30)
+
+
+	SET @codTurma = (SELECT codTurma FROM tbTurma
+		WHERE @nomeTurma = nomeTurma)
+	SET @nomeTurma = (SELECT nomeTurma FROM tbTurma
+		WHERE @nomeTurma = nomeTurma)
+	SET @nomeCurso = (SELECT codTurma FROM tbTurma
+		WHERE @nomeTurma = nomeTurma)
+	SET @horarioTurma = (SELECT horarioTurma FROM tbTurma
+		WHERE @nomeTurma = nomeTurma)
+
+	PRINT(' Código da turma : ' + @codTurma + ' , Nome da Turma ' + @nomeTurma + ' , Curso da Turma : ' + @nomeCurso + ' , Horario da Turma : ' )
+
+--5)
+CREATE PROCEDURE Exibe_AlunosdaTurma
+
+
+
+
+
+
+	
